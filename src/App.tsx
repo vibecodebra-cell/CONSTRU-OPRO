c9108479b084a7ad5bc3281fbbbae395eab8f368
[dyad] <vibecodebra@gmail.com>
2026-03-05 18:57:50 -0300

    [dyad] Silenciando avisos do React Router - wrote 1 file(s) + extra files edited outside of Dyad

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,12 @@ import Proposals from './pages/Proposals';
 
 function App() {
   return (
-    <Router>
+    <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <Layout>
         <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/tools" element={<Tools />} />
-          <Route path="/proposals" element={<Proposals />} />
+          <Route path="/" element={ <Index /> } />
+          <Route path="/tools" element={ <Tools /> } />
+          <Route path="/proposals" element={ <Proposals /> } />
         </Routes>
       </Layout>
     </Router>
